8c451cfabec4d232373ed1885bdc875edb0a00d8
MariuszG <w69783@student.wsiz.edu.pl>
2023-12-17 16:23:48 +0100

    lab4

---
+++ lab4/lab4.py
@@ -0,0 +1,57 @@
+#zad1
+#a)
+# lista=["Kacper", "Anna", "Dawid", "Martyna"]
+# print(sorted(lista))
+# #b)
+# lista.append("Kuba")
+# lista.append("Marek")
+# n=lista.pop()
+# print(n)
+# #c)
+# lista.insert(3, "Hubert")
+# #d)
+# lista.reverse()
+# lista=lista*2
+# print(lista)
+
+#zad2
+# tekst = "Rzeszów jest piękny"
+# print(tekst[0])
+# print(tekst[6: 12: 3], tekst[1])
+
+#zad3
+# tekst ="Python jest super"
+# print(tekst[0])
+# a=len(tekst)-1
+# print(tekst[a])
+# print(tekst[0: a+1: 2])
+# print(tekst[1: a+1: 3])
+# print(tekst[10: a+1])
+# print(tekst[a+1: 0: -1])
+
+#zad4
+# imie=input("Podaj imie: ")
+# print(f"Witaj {imie}")
+# wiek=int(input("Podaj wiek: "))
+# print(f"Twój wiek to: {wiek}")
+# nazwisko=input("Podaj nazwisko: ")
+# pi=imie[0]
+# pn=nazwisko[0]
+# i=pi+pn
+# print(f"Twoje inicjały to: {i}")
+
+# n=input("Podaj ciag znakow: ")
+# n=(n+" ")*5
+# print(n)
+
+# n=input("Podaj ciag znakow: ")
+# n2=input("Podaj drugi ciag znaków:")
+# m=n+n2
+# print(m)
+
+# n=input("Podaj ciag znakow: ")
+# n2=input("Podaj drugi ciag znaków:")
+# ln=int(len(n)/2)
+# ln2=int(len(n2)/2)
+# m=n[:ln]+n2[ln2:]
+# print(m)
